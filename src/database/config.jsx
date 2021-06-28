763e6da4aafa82d4e6b16c9f2b229dd0848b5add
Ibrahem zablah <IbrahemZablah998@Ibrahemzablah1@gmail.com>
2021-06-28 19:00:21 +0300

    started with new branch to setup a connect with firebase

---
+++ src/database/config.jsx
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyACsm9xRZyKm0gQFl1dl5vcs_mXAWqfdN4",
+    authDomain: "school-management-system-2bb33.firebaseapp.com",
+    projectId: "school-management-system-2bb33",
+    storageBucket: "school-management-system-2bb33.appspot.com",
+    messagingSenderId: "1052910738542",
+    appId: "1:1052910738542:web:49c680f2a63c2bd9f85a7f"
+});
+
+export default firebaseApp;
