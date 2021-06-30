12bebade2624bad76baf692818d8b939766d949d
Bashar Qaddumi <basharq@wix.com>
2021-06-30 16:08:21 +0300

    add firebase to package.json file and refactor common components

---
+++ src/components/common/InputField.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import classNames from 'classnames';
+
+import classes from "./InputField.module.css";
+
+const InputField = (props) => {
+  const {
+    label,
+    onChange,
+    type,
+    placeholder,
+    value,
+    valdationRegex,
+    errorEmailMessage,
+    errorPasswordMessage,
+    required,
+    id,
+    checkConfirm,
+  } = props;
+  const isValid = !!value?.match(valdationRegex);
+  const isValidated = !isValid && value !== "";
+  const isConfirm = value === checkConfirm;
+
+  let classEmailValid = classNames({
+    errorInput: isValidated,
+    input: !isValidated},
+  );
+  
+  return (
+    <div>
+      <label className={classes.label}>{label}</label>
+      <input
+        type={type}
+        placeholder={placeholder}
+        onChange={onChange}
+        className={classes[classEmailValid]}
+        value={value}
+        required={required}
+        id={id}
+      />
+      <p className={classes.errorMessage}>
+        {isValidated ? errorEmailMessage : ""}
+      </p>
+      <p className={classes.errorMessage}>
+        {!isConfirm && value !== "" ? errorPasswordMessage : ""}
+      </p>
+    </div>
+  );
+};
+export default InputField;
