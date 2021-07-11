70be8722d52ce1ecd21b7b93eb2587117d43abf5
IZablah <86057253+IZablah@users.noreply.github.com>
2021-07-11 15:11:35 +0300

    Fix warning and bug (#15)
    * Adding cookies session in redux login
    
    * Fix some warning
    
    * Adding a loader when fetch username from firebase
    
    * Fix a bug when refresh page goes to stackover flow
    
    * Create a new page administration by controlling a role users
    
    * Editing path from website
    
    * Fix a bug
    
    * Change on Subnavbar from Administration to userAccount
    
    * Change on Subnavbar from Administration to userAccount
    
    * Fix a warning
    
    Co-authored-by: Ibrahem zablah <IbrahemZablah998@Ibrahemzablah1@gmail.com>

--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -1,13 +1,8 @@
 import { useSelector } from "react-redux";
 import Loader from "../../components/common/loader/loader";
 import classes from "./homePage.module.css";
-import Students from "../students/students";
-import Teachers from "../teachers/teachers";
 
 const HomePage = () => {
-  const currentUserRole = useSelector((state) => state.auth.currentUserRole);
-  const isTeacher = (currentUserRole === "Teachers");
-  const isStudent = (currentUserRole === "Students");
   const isLoading = useSelector((state) => state.loader.isLoading);
 
   return (
