7cbaa362e6e862fdeea9ba9622ce600496d5a92f
Ibrahem zablah <IbrahemZablah998@Ibrahemzablah1@gmail.com>
2021-07-12 09:48:42 +0300

    Adding A dynamic button Show/Hide in Scheduling Teachers

--- src/pages/schedulerTeachers/schedulingTeachers.jsx
+++ src/pages/schedulerTeachers/schedulingTeachers.jsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Table from '../../components/common/Tables/userTable'
 import classes from './schedulingTeachers.module.css';
 
 const SchedulingTeachers = () => {
+    const [changeSheetLevelHandler, setChangeSheetLevelHandler] = useState();
+    const [changeSheetClassHandler, setChangeSheetClassHandler] = useState();
+    const [changeSheetSectionHandler, setChangeSheetSectionHandler] = useState();
+    const [viewSheet, setViewSheet] = useState();
+
     const data = React.useMemo(
         () => [
             {
                 col1: 'Hello',
                 col2: 'World',
+                col3: 'World',
+                col4: 'World',
+                col5: 'World',
             },
             {
                 col1: 'react-table',
                 col2: 'rocks',
+                col3: 'World',
+                col4: 'World',
+                col5: 'World',
             },
             {
                 col1: 'whatever',
                 col2: 'you want',
+                col3: 'World',
+                col4: 'World',
+                col5: 'World',
             },
         ],
         []
@@ -24,12 +38,24 @@ const SchedulingTeachers = () => {
     const columns = React.useMemo(
         () => [
             {
-                Header: 'Column 1',
-                accessor: 'col1', // accessor is the "key" in the data
+                Header: 'Sunday',
+                accessor: 'col1',
             },
             {
-                Header: 'Column 2',
+                Header: 'Monday',
                 accessor: 'col2',
+            },
+            {
+                Header: 'Tuesday',
+                accessor: 'col3',
+            },
+            {
+                Header: 'Wednesday',
+                accessor: 'col4',
+            },
+            {
+                Header: 'Thursday',
+                accessor: 'col5',
             },
         ],
         []
@@ -37,6 +63,19 @@ const SchedulingTeachers = () => {
 
     const onViewSheetHandler = (event) => {
         event.preventDefault();
+        setViewSheet(!viewSheet);
+    };
+
+    const onChangeSheetLevelHandler = (event) => {
+        setChangeSheetLevelHandler(event.target.value)
+    };
+
+    const onChangeSheetClassHandler = (event) => {
+        setChangeSheetClassHandler(event.target.value)
+    };
+
+    const onChangeSheetSectionHandler = (event) => {
+        setChangeSheetSectionHandler(event.target.value)
     };
 
     return (
@@ -45,7 +84,7 @@ const SchedulingTeachers = () => {
                 <h1>Tabulation Sheet Of School</h1>
                 <div className={classes.sectionTabulation}>
                     <div className={classes.section}>Level</div>
-                    <select required className={classes.selectOptionClasses}>
+                    <select required className={classes.selectOptionClassTabulation} onChange={onChangeSheetLevelHandler}>
                         <option></option>
                         <option value="A">A</option>
                         <option value="B">B</option>
@@ -53,7 +92,7 @@ const SchedulingTeachers = () => {
                         <option value="E">E</option>
                     </select>
                     <div className={classes.section}>Class</div>
-                    <select required className={classes.selectOptionClasses}>
+                    <select required className={classes.selectOptionClassTabulation} onChange={onChangeSheetClassHandler}>
                         <option></option>
                         <option value="1st">1st</option>
                         <option value="2st">2st</option>
@@ -61,7 +100,7 @@ const SchedulingTeachers = () => {
                         <option value="4st">4st</option>
                     </select>
                     <div className={classes.section}>Section</div>
-                    <select required className={classes.selectOptionClasses}>
+                    <select required className={classes.selectOptionClassTabulation} onChange={onChangeSheetSectionHandler}>
                         <option></option>
                         <option value="English">English</option>
                         <option value="Arabic">Arabic</option>
@@ -69,14 +108,17 @@ const SchedulingTeachers = () => {
                         <option value="Pieology">Pieology</option>
                     </select>
                     <div className={classes.actions}>
-                        <button className={classes.buttonViewSheet}>View Sheet</button>
+                        <button className={classes.buttonViewSheet}>
+                            {!viewSheet ? 'View Sheet' : 'Hide Sheet'}</button>
                     </div>
                 </div>
             </div>
-            <div className={classes.headerTabulationSheets}>
-                <h1>Tabulation Sheets</h1>
-                <Table columns={columns} data={data} />
-            </div>
+            {viewSheet &&
+                <div className={classes.headerTabulationSheets}>
+                    <h1>Tabulation Sheets</h1>
+                    <Table columns={columns} data={data} />
+                </div>
+            }
         </form>
     );
 };
