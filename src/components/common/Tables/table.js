b8e9b829c592d1ee13b079bd5dbf5ba34fb675e1
TaimaTask <86023184+TaimaTask@users.noreply.github.com>
2021-08-02 15:37:16 +0300

    Add reveal (#39)
    * Use mixin for common style
    
    * Use mixin to reduce code style
    
    * implement sass in our project
    
    * comlete implementing sass
    
    * co implementing sass
    
    * fix conflict
    
    * responsive web design
    
    * refactoring and responsive
    
    * refactoring and responsive
    
    * teacher reponsive and homepage
    
    * Showsup the user name and major in table admin
    
    * store all teachers on collection
    
    * responsive signin-signup
    
    * code refactoring
    
    * refactoring
    
    * code refactoring
    
    * toast when teacher add lecture
    
    * mainnavbar responsive
    
    * Best practice
    
    * Apply slide effect using react-reveal
    
    * Apply slide effect using react-reveal
    
    * paggenation table

--- src/components/common/Tables/table.js
+++ src/components/common/Tables/table.js
@@ -1,57 +1,83 @@
 import React from "react";
-import { useSortBy, useTable } from "react-table";
-import classes from '../../administration/usersTable/usersTable.module.scss';
+import { usePagination, useSortBy, useTable } from "react-table";
+import classes from "../../administration/usersTable/usersTable.module.scss";
 
 const Table = ({ columns, data }) => {
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
     prepareRow,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    nextPage,
+    previousPage,
+    state: { pageIndex },
   } = useTable(
     {
       columns,
       data,
+      initialState: { pageIndex: 0, pageSize: 3 },
     },
-    useSortBy
-  )
+    useSortBy,
+    usePagination
+  );
 
   return (
-    <table {...getTableProps()} className={classes.table}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th className={classes.headerGroup}
-                {...column.getHeaderProps(column.getSortByToggleProps())}
-              >
-                {column.render('Header')}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
-          prepareRow(row)
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return (
-                  <td className={classes.headerGroup}
-                    {...cell.getCellProps()}
-                  >
-                    {cell.render('Cell')}
-                  </td>
-                )
-              })}
+    <>
+      <table {...getTableProps()} className={classes.table}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th
+                  className={classes.headerGroup}
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                >
+                  {column.render("Header")}
+                </th>
+              ))}
             </tr>
-          )
-        })}
-      </tbody>
-    </table>
-  )
-}
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td
+                      className={classes.headerGroup}
+                      {...cell.getCellProps()}
+                    >
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div className="pagination">
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {"<"}
+        </button>{" "}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {">"}
+        </button>{" "}
+        <span>
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{" "}
+        </span>
+      </div>
+    </>
+  );
+};
 
 export default Table;
